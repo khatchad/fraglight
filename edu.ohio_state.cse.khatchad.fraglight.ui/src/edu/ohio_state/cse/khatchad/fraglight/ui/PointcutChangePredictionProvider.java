d5b5943312cedeac445d9725d27c73a425cfa63d
khatchad <khatchad@63390127-f442-c194-f360-8bdfb6b9c77c>
2010-03-23 22:03:50 +0000

    Incomplete - task : Analyze pointcuts upon task activation

--- edu.ohio_state.cse.khatchad.fraglight.ui/src/edu/ohio_state/cse/khatchad/fraglight/ui/PointcutChangePredictionProvider.java
+++ edu.ohio_state.cse.khatchad.fraglight.ui/src/edu/ohio_state/cse/khatchad/fraglight/ui/PointcutChangePredictionProvider.java
@@ -3,11 +3,30 @@
  */
 package edu.ohio_state.cse.khatchad.fraglight.ui;
 
+import static java.lang.System.out;
+import static org.eclipse.core.resources.ResourcesPlugin.getWorkspace;
+
+import java.util.Collection;
+
+import org.eclipse.ajdt.core.javaelements.AdviceElement;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
+import org.eclipse.jdt.core.IJavaElement;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.mylyn.context.core.ContextChangeEvent;
+import org.eclipse.mylyn.context.core.ContextCore;
 import org.eclipse.mylyn.context.core.IInteractionElement;
+import org.eclipse.mylyn.internal.context.core.ContextCorePlugin;
 import org.eclipse.mylyn.internal.context.core.IActiveSearchOperation;
 import org.eclipse.mylyn.internal.java.ui.JavaStructureBridge;
 import org.eclipse.mylyn.internal.java.ui.search.AbstractJavaRelationProvider;
+
+import uk.ac.lancs.comp.khatchad.rejuvenatepc.core.PointcutAnalyzer;
+
+import edu.ohio_state.cse.khatchad.ajplugintools.util.AJUtil;
 
 /**
  * @author <a href="mailto:khatchad@cse.ohio-state.edu">Raffi Khatchadourian</a>
@@ -19,6 +38,8 @@ public class PointcutChangePredictionProvider extends AbstractJavaRelationProvid
 	private static final String ID = ID_GENERIC + ".pointcutchangeprediction";
 	
 	public static final String NAME = "may break"; //$NON-NLS-1$
+	
+	private PointcutAnalyzer analyzer = new PointcutAnalyzer();
 
 	public PointcutChangePredictionProvider() {
 		super(JavaStructureBridge.CONTENT_TYPE, ID);
@@ -26,8 +47,31 @@ public class PointcutChangePredictionProvider extends AbstractJavaRelationProvid
 
 	@Override
 	public void contextChanged(ContextChangeEvent event) {
-		//TODO: This will have to be overrided.
-		System.out.println("Hello from " + this.getClass().getName() + "contextChanged");
+		switch(event.getEventKind()) {
+			case ACTIVATED: {
+				IWorkspace workspace = getWorkspace();
+				IWorkspaceRoot root = workspace.getRoot();
+				IProject[] projects = root.getProjects();
+				for ( IProject proj : projects ) {
+					if ( proj.isAccessible() ) {
+						IJavaProject javaProject = JavaCore.create(proj);
+						Collection<? extends AdviceElement> toAnalyze = null;
+						try {
+							toAnalyze = AJUtil.extractValidAdviceElements(javaProject);
+						}
+						catch (JavaModelException e) {
+							//next project.
+							continue;
+						}
+						if (!toAnalyze.isEmpty()) {
+							this.analyzer.analyze(toAnalyze, lMonitor);
+						}
+					}
+				}
+				
+				break;
+			}
+		}
 	}
 
 	@Override
